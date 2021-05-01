c7cc5a1e45cfb78b4956dd17878c9d131e82eafe
0xzx <80919471+0xzx@users.noreply.github.com>
2021-05-01 10:53:07 +0800

    Create coinswap.js

---
+++ js/coinswap.js
@@ -0,0 +1,6 @@
+document.writeln("<style>");
+document.writeln(".coinswap {border: 2px solid #26e269;padding: 5px 10px;background: #acfdcd;font-weight: 700;border-radius: 20px;}");
+document.writeln(".coinswap:hover{border: 2px solid #6374ec;padding: 5px 10px;background: #c0d6f1;}");
+document.writeln("</style>");
+document.writeln("<li><a href=\'https://master.coinswap.space/farm?coinswapfriend=0xA007938E1Cd29B4CF94DF0F1BAFe64Eb7C356783\' rel=\'noreferrer nofollow external noopener\' target=\'_blank\'><span  class=\'coinswap\'>SWAP</span></a>");
+document.writeln("</li>");
